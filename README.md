51671ee5edeb72a303355b4d232575bdb57b58eb
Hiroskey <hiroki.nishikawa.pbl@gmail.com>
2021-02-21 02:34:47 +0900

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # codes for aizu online judge
+x_cube: write a program which calculates the cube of a given integer x
---
+++ push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git status
+git add .
+git commit -m "new commit"
+git branch
+git remote add origin git@github.com:Hiroskey/practice_programming.git
+git push -u origin master
+
+# git remote rm origin
+# git remote add origin git@github.com:Hiroskey/practice_programming.git
+# git push -u origin master
+
